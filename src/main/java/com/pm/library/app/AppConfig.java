2f08aac5b1b693419e6b7dc3383f222fd25335e6
Prapangkorn Thangsathityangkul <XxtheNerdLordxX@outlook.com>
2025-08-20 21:27:11 +0700

    chore(app): wire components in AppConfig and demo Main using LibraryService

--- src/main/java/com/pm/library/app/AppConfig.java
+++ src/main/java/com/pm/library/app/AppConfig.java
@@ -1,4 +1,22 @@
 package com.pm.library.app;
 
-public class AppConfig {
+import com.pm.library.adapters.logging.LoggingBookRepository;
+import com.pm.library.adapters.repo.InMemoryBookRepository;
+import com.pm.library.domain.validation.BookValidator;
+import com.pm.library.ports.BookRepository;
+import com.pm.library.adapters.id.UuidGenerator;
+import com.pm.library.ports.IdGenerator;
+import com.pm.library.services.LibraryService;
+
+public final class AppConfig {
+	private AppConfig() {}
+
+	public static LibraryService libraryService() {
+		InMemoryBookRepository repo = new InMemoryBookRepository();
+		BookRepository loggingRepo = new LoggingBookRepository(repo);
+		BookValidator validator = new BookValidator();
+		IdGenerator idGen = new UuidGenerator();
+
+		return new LibraryService(loggingRepo, validator, idGen);
+	}
 }
--- src/main/java/com/pm/library/app/Main.java
+++ src/main/java/com/pm/library/app/Main.java
@@ -1,9 +1,19 @@
 package com.pm.library.app;
 
-import com.pm.library.domain.Book;
+import com.pm.library.services.LibraryService;
+import com.pm.library.services.dto.BookCreateRequest;
+import com.pm.library.shared.Result;
+import com.pm.library.services.dto.BookView;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("001", "Book 1", "James", true);
+        LibraryService svc = AppConfig.libraryService();
+
+        Result<BookView> res = svc.add(new BookCreateRequest("Effective Java", "Joshua Bloch"));
+        if (res.isOk()) {
+            System.out.println("Added: " + res.getValue().title() + " by " + res.getValue().author());
+        } else {
+            System.err.println("Failed to add book: " + res.errorCode() + " - " + res.message());
+        }
     }
 }
